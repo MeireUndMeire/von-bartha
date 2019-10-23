49463d86e019b3bc6720d35fc20e952dc0c28020
Jérôme Braeken <jeromebraeken@gmail.com>
2019-10-23 23:03:26 +0200

    fixing last push

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const DEPLOY_ENV = process.env.DEPLOY_ENV || 'lbn_published_production';
+const DEPLOY_ENV = process.env.DEPLOY_ENV || 'lbn_published_production' || 'lbn_published_stage';
 
 /**
  * Generate node edges
@@ -13,49 +13,7 @@ exports.onCreateNode = ({ node, actions }) => {
    */
   if (
     !Object.prototype.hasOwnProperty.call(node, 'meta') ||
-    !Object.prototype.hasOwnProperty.call(node.meta, 'lbn_published_production')
-    ) {
-    createNodeField({ node, name: 'deploy', value: true });
-    return;
-  }
-
-  let deploy;
-
-  if (node.meta[DEPLOY_ENV]) {
-    deploy = true;
-  } else {
-    deploy = false;
-  }
-
-  createNodeField({ node, name: 'deploy', value: deploy });
-};
-
-
-const createPosts = require('./gatsby/createPosts');
-const createPages = require('./gatsby/createPages');
-const createCategories = require('./gatsby/createCategories');
-
-exports.createPages = async ({ actions, graphql }) => {
-  await createPosts({ actions, graphql });
-  await createPages({ actions, graphql });
-  await createCategories({ actions, graphql });
-}
-
-const DEPLOY_ENV = process.env.DEPLOY_ENV || 'lbn_published_stage';
-
-/**
- * Generate node edges
- *
- * @param {any} { node, actions, getNode }
- */
-exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
-
-  /**
-   * If these don't exist, the LBN WordPress Plugin isn't installed – so build all posts.
-   */
-  if (
-    !Object.prototype.hasOwnProperty.call(node, 'meta') ||
+    !Object.prototype.hasOwnProperty.call(node.meta, 'lbn_published_production') ||
     !Object.prototype.hasOwnProperty.call(node.meta, 'lbn_published_stage')
     ) {
     createNodeField({ node, name: 'deploy', value: true });
