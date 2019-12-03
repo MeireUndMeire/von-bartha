35e723e4f16a38d5a2f34c66fab18aaebf8e9d4f
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-03 15:52:16 +0100

    Exhibition detail (mainly gallery)

--- src/templates/exhibition.js
+++ src/templates/exhibition.js
@@ -9,46 +9,48 @@ const Gallery = styled.div`
   flex-direction: row;
   flex-wrap: nowrap;
   flex-direction: flex-start;
+  width: auto;
+  margin-bottom: 50px;
+  overflow-x: scroll;
 
-  margin-bottom: 150px;
-
-  &:first-child{
-    margin-left: 50%;
+  #slide0 {
+    margin-left: 50vw;
     transform: translateX(-50%);
   }
+
   .slide {
-    margin-left: 20px;
-    min-width: 60vw;
-    height: 400px;
+    transform: translateX(-50%);
+    padding-left: 40px;
+    margin-left: 0;
     img {
-      height: inherit;
-      object-fit: cover;
+      width: auto;
+      height: 50vh;
+      object-fit: contain;
     }
-  }
-  .spacer {
-    width: 200px;
-    flex: 0 1 auto;
   }
   .bold > p {
     font-family: 'Trade-Gothic';
     font-size: 1rem;
+    line-height: .5rem;
   }
   .bold {
     padding: 10px 0 0 70px;
   }
 `
 
-const Links = styled.div`
+const Linkss = styled.div`
   width: 100%;
-  height: 100px;
+  height: 100%;
   display: flex;
   .pressRelease {
     width: 50%;
+    height: 150px;
     background-color: #D1FFC3;
   }
   .artistProfile {
     width: 50%;
     background-color: #B2BFC8;
+    height: 150px;
   }
   h2 {
     padding: 10px;
@@ -65,34 +67,36 @@ const ExhibitionTemplate = (props) => {
   const exhibition = props.data.allWordpressWpExhibitions.edges[0].node
   const slides = exhibition.acf.gallery_module_exhibitions[0].slides
 
+  console.log(slides[0])
+
   
   return (
     <Layout>
       <div className="exhibition">
         <h1>{exhibition.title}</h1>
         <h2>{exhibition.acf.exhibition_subtitle}</h2>
-        <img className="fullwidth" alt={exhibition.acf.fullwidth_image.title} src={exhibition.acf.fullwidth_image.source_url} />
+        <img className="fullWidth" alt={exhibition.acf.fullwidth_image.title} src={exhibition.acf.fullwidth_image.source_url} />
         <h2>{exhibition.acf.starting_date} - {exhibition.acf.ending_date}</h2>
         <h2>{exhibition.acf.exhibition_location}</h2>
         <Textblock><div dangerouslySetInnerHTML={{ __html: exhibition.acf.textblock }}></div></Textblock>
-        <Gallery className="slides">
-            <div className="spacer"></div>
-            {slides.map(slide => (
-              
-                <div className="slide">
+        <Gallery className="slides fullWidth">
+            {slides.map((slide, index) => (
+                <div className="slide" key={index} id={'slide' + index}>
                   <img src={slide.image.source_url} alt={slide.image.title} />
                   <div className="bold" dangerouslySetInnerHTML={{ __html: slide.caption }}></div>
                 </div>
             ))}
         </Gallery>
-        <Links className="fullWidth">
-              <div className="artistProfile" >
-                <Link to={exhibition.artist}><h2>Artists Profile</h2></Link>
-              </div>
+        <Linkss className="fullWidth">
+              {exhibition.acf.artist_name.post_name =! 'undefined' &&
+                <div className="artistProfile" >
+                  <Link to={exhibition.acf.artist_name.post_name}><h2>Artists Profile</h2></Link>
+                </div>  
+              }
               <div className="pressRelease">
                 <a href={exhibition.acf.press_release.source_url} download target="_blank"><h2>Press Release</h2></a>
               </div>
-        </Links>
+        </Linkss>
       </div>
     </Layout>
   )
