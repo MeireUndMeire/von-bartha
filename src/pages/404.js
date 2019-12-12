16123280c262dfd5479498d35eef281c4925254e
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-12 15:21:27 +0100

    404 stupid joke

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,33 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from '../components/layout'
 
 
 const NotFoundPage = (props) => {
   const siteTitle = props.data.site.siteMetadata.title;
 
+  const Four = styled.div`
+  img {
+    width: 50vw;
+  }
+  `
+  const Back = styled.div`
+
+  `
+
   return (
-    <div>
-      <h1>This - {siteTitle} page - cannot be not found</h1>
-      <p>Maybe it's taking a barth?</p>
-    </div>
+    <Layout>
+      <Four>
+        <h1 className="overviewHeading">This - {siteTitle} page - cannot be not found</h1>
+        <h2>Maybe it's taking a barth?</h2>
+        <img alt="bath" src="https://previews.123rf.com/images/paperandpixels/paperandpixels1711/paperandpixels171100006/89398479-bath-tub-line-icon-in-pixel-style-linear-symbol-of-a-bathtub-with-shower-head-turned-on-pouring-wate.jpg"></img>
+      </Four>
+      <Back>
+        <Link to="/" className="backLink"><h2>&#8592; Back</h2></Link>
+      </Back>
+    </Layout>
   )
 }
 
