05edd23f74825f2e463c1c1d2e9ec9c4bff9ce71
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-13 00:39:31 +0100

    Agenda filtering aplied

--- src/pages/agenda/index.js
+++ src/pages/agenda/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
+import moment from 'moment'
 
-import Layout from '../../components/layout';
+import Layout from '../../components/layout'
 import styled from "styled-components"
 
 const Header = styled.h1`
@@ -31,6 +32,7 @@ const Back = styled.div`
 
 const AgendaPage = (props) => {
     const allEvents = props.data.allWordpressWpEvents.edges
+    let today = moment(new Date()).format('YYYYMMDD')
 
     return (
 
@@ -40,50 +42,79 @@ const AgendaPage = (props) => {
             {/* on view */}
             <Heading className="overviewHeading"><h1>now</h1></Heading>
             <OnViewWrapper className="OnViewWrapper">
-            {allEvents.map(event => (
+            {allEvents.map(event => {
+                const currentStatus = today - event.node.acf.startingDateNoFormat
+                const duration = event.node.acf.endingDateNoFormat - event.node.acf.startingDateNoFormat
+                if (currentStatus >= 0 && currentStatus <= duration) {
+                    return (
                      <EventItem className="two-grid-item" key={event.node.id}>
                                 <Link className="link" to={event.node.path}>
                                     <p className="date-small">Until {event.node.acf.starting_date}</p>
-                                    <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
+                                    {event.node.acf.fullwidth_image != null &&
+                                        <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
+                                    }
                                     <h1>{event.node.acf.event_name}</h1>
+                                    {event.node.acf.event_subtitle != null &&
                                     <h2>{event.node.acf.event_subtitle}</h2>
+                                    }
                                 </Link>
                     </EventItem>
-            ))}
+                    )
+                }
+            })}
             </OnViewWrapper>
 
             {/* soon */}
             <Heading className="overviewHeading"><h1>soon</h1></Heading>
             <OnViewWrapper className="OnViewWrapper">
-            {allEvents.map(event => (
+            {allEvents.map(event => {
+                const currentStatus = today - event.node.acf.startingDateNoFormat
+                if (currentStatus < 0 ) {
+                    return(
                      <EventItem className="two-grid-item" key={event.node.id}>
                                 
                                 {/* {event.node.acf.starting_date ? 'undefined' : 'not'} */}
                                 <Link className="link" to={event.node.path}>
                                     <p className="date-small">{event.node.acf.starting_date}</p>
-                                    <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
+                                    {event.node.acf.fullwidth_image != null &&
+                                        <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
+                                    }
                                     <h1>{event.node.acf.event_name}</h1>
-                                    <h2>{event.node.acf.event_subtitle}</h2>
+                                    {event.node.acf.event_subtitle != null &&
+                                        <h2>{event.node.acf.event_subtitle}</h2>
+                                    }
                                 </Link>
                             
                     </EventItem>
-            ))}
+                    )
+                }
+            })}
             </OnViewWrapper>
             
 
             {/* past */}
             <Heading className="overviewHeading"><h1>past</h1></Heading>
             <OnViewWrapper className="OnViewWrapper">
-            {allEvents.map(event => (
-                <EventItem className="two-grid-item" key={event.node.id}>
-                    <Link className="link" to={event.node.path}>
-                        <p className="date-small">{event.node.acf.starting_date}</p>
-                        <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
-                        <h1>{event.node.acf.event_name}</h1>
-                        <h2>{event.node.acf.event_subtitle}</h2>
-                    </Link> 
-                </EventItem>
-            ))}
+            {allEvents.map(event => {
+                const currentStatus = today - event.node.acf.startingDateNoFormat
+                const duration = event.node.acf.endingDateNoFormat - event.node.acf.startingDateNoFormat
+                if (currentStatus >= 0 && currentStatus > duration) {
+                    return (
+                        <EventItem className="two-grid-item" key={event.node.id}>
+                            <Link className="link" to={event.node.path}>
+                                <p className="date-small">{event.node.acf.starting_date}</p>
+                                {event.node.acf.fullwidth_image != null &&
+                                    <img alt={event.node.acf.fullwidth_image.title} src={event.node.acf.fullwidth_image.source_url} />
+                                }
+                                <h1>{event.node.acf.event_name}</h1>
+                                {event.node.acf.event_subtitle != null &&
+                                    <h2>{event.node.acf.event_subtitle}</h2>
+                                }
+                            </Link> 
+                        </EventItem>
+                    )
+                }
+            })}
             </OnViewWrapper>
             
             <Back>
@@ -97,7 +128,7 @@ export default AgendaPage
 
 export const EventsQuery = graphql`
 {
-    allWordpressWpEvents {
+    allWordpressWpEvents(sort: {order: DESC, fields: acf___starting_date}) {
         edges {
             node {
                 id
@@ -107,6 +138,8 @@ export const EventsQuery = graphql`
                         source_url
                         title
                     }
+                    startingDateNoFormat: starting_date
+                    endingDateNoFormat: ending_date
                     starting_date(formatString: "MMM DD")
                     ending_date(formatString: "MMM DD YYYY")
                     event_subtitle
