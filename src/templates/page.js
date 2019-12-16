0ff58be4f402bec6f5ae3f06eefa8f939225caea
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-16 09:38:11 +0100

    Adjustments page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -5,46 +5,26 @@ import Layout from "../components/layout"
 
 const PageTemplate = (props) => {
 
-  // const post = props.data.wordpressPost;
-  // const siteTitle = props.data.site.siteMetadata.title;
-  // let featuredImage = false;
-
-  // if (post.featured_media && post.featured_media.source_url ) {
-  //   featuredImage = post.featured_media.source_url;
-  // }
 
   return (
     <Layout location={props.location}></Layout>
-    //<Layout location={props.location} title={siteTitle}></Layout>
   )
 
 }
 
 export default PageTemplate
 
-// export const pageQuery = graphql`
-//   query PostById($id: String!) {
-//     site {
-//       siteMetadata {
-//         title
-//         author
-//       }
-//     }
-//     wordpressPost(id: { eq: $id }) {
-//       date(formatString: "MMMM DD, YYYY")
-//       slug
-//       title
-//       modified
-//       excerpt
-//       id
-//       # featured_media {
-//       #  source_url
-//       # }
-//       categories {
-//         name
-//         slug
-//       }
-//       content
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          title
+          slug
+          id
+          template
+        }
+      }
+    }
+  }
+`
