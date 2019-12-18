37ec36870f8b97f356e7d39cd597b5ea472164a7
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-18 21:36:55 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,6 +12,9 @@ const NotFoundPage = (props) => {
   img {
     width: 50vw;
   }
+  h2 {
+    text-transform: none;
+  }
   `
   const Back = styled.div`
 
@@ -20,8 +23,8 @@ const NotFoundPage = (props) => {
   return (
     <Layout>
       <Four>
-        <h1 className="overviewHeading">This - {siteTitle} page - cannot be not found</h1>
-        <h2>Maybe it's taking a barth?</h2>
+        <h1 className="overviewHeading">This - {siteTitle} page - can't be not found</h1>
+        <h2>Maybe it's taking a Barth?</h2>
         <img alt="bath" src="https://previews.123rf.com/images/paperandpixels/paperandpixels1711/paperandpixels171100006/89398479-bath-tub-line-icon-in-pixel-style-linear-symbol-of-a-bathtub-with-shower-head-turned-on-pouring-wate.jpg"></img>
       </Four>
       <Back>
