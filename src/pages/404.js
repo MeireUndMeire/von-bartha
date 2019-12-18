061266486101e13a8a7b086fdc8a61dfe8842f43
Jérôme Braeken <jeromebraeken@gmail.com>
2019-12-18 21:38:33 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -23,7 +23,7 @@ const NotFoundPage = (props) => {
   return (
     <Layout>
       <Four>
-        <h1 className="overviewHeading">This - {siteTitle} page - can't be not found</h1>
+        <h1 className="overviewHeading">This - {siteTitle} page - can't be found</h1>
         <h2>Maybe it's taking a Barth?</h2>
         <img alt="bath" src="https://previews.123rf.com/images/paperandpixels/paperandpixels1711/paperandpixels171100006/89398479-bath-tub-line-icon-in-pixel-style-linear-symbol-of-a-bathtub-with-shower-head-turned-on-pouring-wate.jpg"></img>
       </Four>
