4af9ccb32962f8a0ca45bc1adca30bb7a2801901
Jérôme Braeken <jeromebraeken@gmail.com>
2020-01-05 21:25:19 +0100

    Adding fragment for custom colors

--- src/components/fragments/fragments.js
+++ src/components/fragments/fragments.js
@@ -57,7 +57,93 @@ export const eventFragment = graphql`
       event_location
       event_name
       event_subtitle
-      fullwidth_image
+      custom_color
+      color_scheme {
+        acid_blue_on_clay_ash_green {
+          acid_blue
+          ash_green
+        }
+        acid_blue_on_haiti_violet_ {
+          acid_blue
+          haiti_violet
+        }
+        acid_blue_on_heather_blue {
+          acid_blue
+          heather_blue
+        }
+        acid_blue_on_tana_beige {
+          acid_blue
+          tana_beige
+        }
+        acid_green_on_fern_green {
+          acid_green
+          fern_green
+        }
+        value
+        acid_green_on_haiti_violet {
+          acid_green
+          haiti_violet
+        }
+        acid_green_on_oyster_pink {
+          acid_green
+          oyster_pink
+        }
+        acid_green_on_topaz_violet {
+          acid_green
+          topaz_violet
+        }
+        acid_yellow_on_clay_ash_green {
+          acid_yellow
+          clay_ash_green
+        }
+        acid_yellow_on_haiti_violet {
+          acid_yellow
+          haiti_violet
+        }
+        acid_yellow_on_tana_beige {
+          acid_yellow
+          tana_beige
+        }
+        acid_yellow_on_vermilion_red {
+          acid_yellow
+          vermilion_red
+        }
+        celadon_green_on_haiti_violet {
+          celadon_green
+          haiti_violet
+        }
+        celadon_green_on_heather_blue {
+          celadon_green
+          heather_blue
+        }
+        celadon_green_on_koromiko_orange {
+          celadon_green
+          koromiko_orange
+        }
+        celadon_green_on_vermilion_red {
+          celadon_green
+          vermilion_red
+        }
+        vermilion_red_on_clay_ash_green {
+          clay_ash_green
+          vermilion_red
+        }
+        vermilion_red_on_haiti_violet {
+          haiti_violet
+          vermilion_red
+        }
+        vermilion_red_on_tana_beige {
+          tana_beige
+          vermilion_red
+        }
+        vermilion_red_on_topaz_violet {
+          topaz_violet
+          vermilion_red
+        }
+      }
+      fullwidth_image {
+        source_url
+      }
       gallery_module_events {
         slides {
           image {
